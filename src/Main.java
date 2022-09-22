5b781936a0c89dce21ff16fe48af511800152a9a
Chevez Robertson <chai@dhcp-172-30-225-209.guest.uc.edu>
2022-09-22 15:03:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter your age");
+
+        int age = 20;
+
+        if(age >= 21){
+            System.out.println("You get a wristband! ");
+        }
+
+    }
+}
